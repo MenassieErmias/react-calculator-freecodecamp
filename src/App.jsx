d7947c2c6b89cc9ad656d0d003d699340ee2ae06
MenassieErmias <ermiasmenase@gmail.com>
2023-10-14 14:13:14 +0300

    basic fucntionality added, plenty of bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
 function App() {
+  // STATES
   const [display, setDisplay] = useState("0");
 
+  // FUNCTIONS
+  const onDigitPress = (event) => {
+    if (display == "0") {
+      setDisplay(event.target.value)
+    } else {
+      setDisplay(prev => prev + event.target.value)
+    }
+  }
+
+  const onEqualsPress = () => {
+    setDisplay(prev => eval(prev))
+  }
+
+  const onBackspacePress = () => {
+    setDisplay(prev => prev.slice(0, -1))
+  }
+
+  const onClearPress = () => {
+    setDisplay("0")
+  }
   return (
     <div className="calculator">
       <div className="display">
@@ -9,27 +30,28 @@ function App() {
       </div>
       <div className="inputs">
         <div className="digits">
-          <button id='nine'>9</button>
-          <button id='eight'>8</button>
-          <button id='seven'>7</button>
-          <button id='six'>6</button>
-          <button id='five'>5</button>
-          <button id='four'>4</button>
-          <button id='three'>3</button>
-          <button id='two'>2</button>
-          <button id='one'>1</button>
-          <button id='zero'>0</button>
+          <button id='nine' value='9' onClick={onDigitPress}>9</button>
+          <button id='eight' value='8' onClick={onDigitPress}>8</button>
+          <button id='seven' value='7' onClick={onDigitPress}>7</button>
+          <button id='six' value='6' onClick={onDigitPress}>6</button>
+          <button id='five' value='5' onClick={onDigitPress}>5</button>
+          <button id='four' value='4' onClick={onDigitPress}>4</button>
+          <button id='three' value='3' onClick={onDigitPress}>3</button>
+          <button id='two' value='2' onClick={onDigitPress}>2</button>
+          <button id='one' value='1' onClick={onDigitPress}>1</button>
+          <button id='zero' value='0' onClick={onDigitPress}>0</button>
+          <button id='decimal' value='.' onClick={onDigitPress}>.</button>
         </div>
         <div className="operations">
-          <button id='add'>+</button>
-          <button id='subtract'>-</button>
-          <button id='multiply'>*</button>
-          <button id='divide'>/</button>
+          <button id='add' value='+' onClick={onDigitPress}>+</button>
+          <button id='subtract' value='-' onClick={onDigitPress}>-</button>
+          <button id='multiply' value='*' onClick={onDigitPress}>*</button>
+          <button id='divide' value='/' onClick={onDigitPress}>/</button>
         </div>
         <div className="action">
-          <button id='equals'>=</button>
-          <button id='clear'>CE</button>
-          <button id='backspace'>&#x2190;</button>
+          <button id='equals' onClick={onEqualsPress}>=</button>
+          <button id='clear' onClick={onClearPress}>CE</button>
+          <button id='backspace' onClick={onBackspacePress}>&#x2190;</button>
         </div>
       </div>
     </div>
